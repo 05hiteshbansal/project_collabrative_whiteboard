39af4a45d87d2c6d2ce83803e1d59a0d1d6a8f9f
Hitesh_Bansal <h.hiteshbansal@gmail.com>
2023-01-07 00:17:21 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+var r;
+let userid = {}; // 1 2 3 4
+//var roomid={} // A-> 2,3 | B -> 1,4
+app.use("/", express.static(__dirname + "/public"));
+io.on("connection", (soc) => {
+    console.log("connection on", soc.id);
+    soc.on("id", (data) => {
+        r = data.username;
+        soc.join(r);
+
+        if (userid[r] >= 1) {
+            userid[r]++;
+            console.log("1");
+        } else {
+            userid[r] = 1;
+            console.log("2");
+        }
+        console.log(data.username);
+        soc.emit("data_send", data);
+    });
+
+    soc.on("draw", (data) => {
+        // console.log(data.input);
+        console.log("msg_data 1");
+        console.log(data.username);
+        io.to(data.username).emit("msg_send", data); //.in(data.username)
+    });
+});
+
+server.listen(4444, () => {
+    console.log("server is started");
+});
